8371c4a2859a81bf16f3a2d4bc73472e8cf02ff2
prabhat.singh <prabhat.singh@thoughworks.com>
2024-02-14 22:29:03 +0530

    called currency conversion service from convert currency service

---
+++ src/main/java/com/convertcurrency/convertcurrency/resource/ConvertCurrencyController.java
@@ -0,0 +1,34 @@
+package com.convertcurrency.convertcurrency.resource;
+
+import com.convertcurrency.convertcurrency.model.ConvertedCurrency;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class ConvertCurrencyController {
+
+    @GetMapping("/convert-currency/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
+    public ConvertedCurrency getConvertedCurrency(@PathVariable String fromCurrency, @PathVariable String toCurrency, @PathVariable BigDecimal quantity) {
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", fromCurrency);
+        uriVariables.put("to", toCurrency);
+        ResponseEntity<ConvertedCurrency> forEntity =
+                new RestTemplate()
+                        .getForEntity("http://localhost:8000/currency-conversion/from/USD/to/INR",
+                                ConvertedCurrency.class,
+                                uriVariables);
+        ConvertedCurrency convertedCurrency = forEntity.getBody();
+        return new ConvertedCurrency(convertedCurrency.getId(),
+                fromCurrency, toCurrency, quantity,
+                convertedCurrency.getConversionMultiple(),
+                quantity.multiply(convertedCurrency.getConversionMultiple()),
+                convertedCurrency.getEnvironment());
+    }
+
+}
