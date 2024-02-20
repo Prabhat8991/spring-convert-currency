9262dae99a8cd222437bc664095d4acbceabbeef
prabhat.singh <prabhat.singh@thoughworks.com>
2024-02-20 17:53:47 +0530

    implemented Load balancing

--- src/main/java/com/convertcurrency/convertcurrency/proxy/CurrencyConversionProxy.java
+++ src/main/java/com/convertcurrency/convertcurrency/proxy/CurrencyConversionProxy.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-conversion", url = "localhost:8000")
+//@FeignClient(name = "currency-conversion", url = "localhost:8000")
+@FeignClient(name = "currency-conversion")
+// on removing url this will talk to eureka and get instance and load balance between them
 public interface CurrencyConversionProxy {
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}")
